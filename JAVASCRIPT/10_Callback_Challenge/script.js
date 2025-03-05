caa8d5aed3200293817e7af3695b5e4e3f3b195d
Kim Subang <kimsubang1@gmail.com>
2025-03-05 08:49:25 +0800

    Refactoring the filter function

--- JAVASCRIPT/10_Callback_Challenge/script.js
+++ JAVASCRIPT/10_Callback_Challenge/script.js
@@ -1,78 +1,89 @@
 // Custom Map Function
-function customMap(arr, callback){
-    for(let x =0; x < arr.length;x++){
-        arr[x] = callback(arr[x])
-    }
-    return arr
+function customMap(arr, callback) {
+  for (let x = 0; x < arr.length; x++) {
+    arr[x] = callback(arr[x]);
+  }
+  return arr;
 }
 
 let numbers = [1, 2, 3];
-let doubled = customMap(numbers, function(num) { return num * 2; });
+let doubled = customMap(numbers, function (num) {
+  return num * 2;
+});
 console.log(doubled); // Should output: [2,4,6]
 
 // Filter Function
-function filter(arr, callback){
-    for(let x in arr){
-        if(!callback(arr[x])){
-            arr.pop(x)
-        }
+function filter(arr, callback) {
+  //Refactored filter function into creating filteredArr
+  let filteredArr = [];
+  for (let x in arr) {
+    if (callback(arr[x])) {
+      filteredArr.push(arr[x]);
     }
-    return arr
+  }
+  return filteredArr;
 }
 
-let result = filter([1,2,3,4,15], function(val) { return val<10; });
+let result = filter([1, , 17, 2, 3, 4, 15], function (val) {
+  return val > 10;
+});
 console.log(result); // Should output: [1,2,3,4]
 
-
 // Some Function
-function some(arr,callback){
-    let output = false
-    for(let x in arr){
-        if(callback(arr[x])){
-            output = true
-        }
+function some(arr, callback) {
+  let output = false;
+  for (let x in arr) {
+    if (callback(arr[x])) {
+      output = true;
     }
-    return output
+  }
+  return output;
 }
-let result2 = some([1,2,3,4], function(val) { return val>5; });
+let result2 = some([1, 2, 3, 4], function (val) {
+  return val > 5;
+});
 console.log(result2); // Should output: true
 
 // Every Function
-function every(arr, callback){
-    let output = false;
-    for(let x in arr){
-        if(callback(arr[x])){
-            output = true
-        }else{
-            output = false
-        }
+function every(arr, callback) {
+  let output = false;
+  for (let x in arr) {
+    if (callback(arr[x])) {
+      output = true;
+    } else {
+      output = false;
     }
-    return output
+  }
+  return output;
 }
-let result3 = every([1,2,3], function(val) { return val>0; });
+let result3 = every([1, 2, 3], function (val) {
+  return val > 0;
+});
 console.log(result3); // Should output: true
 
 // Reduce Function
-function reduce(arr, callback){
-    let total = 0;
-    for(let x in arr){
-        total+= callback(0,arr[x])
-    }
-    return total
+function reduce(arr, callback) {
+  let total = 0;
+  for (let x in arr) {
+    total += callback(0, arr[x]);
+  }
+  return total;
 }
-let sum = reduce([1,2,3], function(acc, num) { return acc + num; });
+let sum = reduce([1, 2, 3], function (acc, num) {
+  return acc + num;
+});
 console.log(sum); // Should output: 6
 
-
 // Include Function
-function includes(arr, callback){
-    for(let x in arr){
-        if(callback(arr[x])){
-            return true
-        }
+function includes(arr, callback) {
+  for (let x in arr) {
+    if (callback(arr[x])) {
+      return true;
     }
-    return false
+  }
+  return false;
 }
-let result4 = includes([1,2,3], function(val) { return val===2; });
+let result4 = includes([1, 2, 3], function (val) {
+  return val === 2;
+});
 console.log(result4); // Should output: true
-

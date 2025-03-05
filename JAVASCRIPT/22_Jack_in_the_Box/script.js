3a829c73b3ea89889a250ba873291ceca9190e96
Kim Subang <kimsubang1@gmail.com>
2025-03-05 22:49:18 +0800

    Updating Jack in the Box Code with animation the bug fix

--- JAVASCRIPT/22_Jack_in_the_Box/script.js
+++ JAVASCRIPT/22_Jack_in_the_Box/script.js
@@ -4,6 +4,7 @@ const img = document.querySelector("img");
 
 for (let x = 0; x < main.childElementCount; x++) {
   main.children[x].addEventListener("click", function () {
+    const createAudio = document.createElement("audio");
     let secIndex;
 
     if (x == 0) {
@@ -14,19 +15,42 @@ for (let x = 0; x < main.childElementCount; x++) {
     const secElement = main.children[secIndex];
     const tempSrc2 = secElement.src;
 
-    console.log(tempSrc2);
-
     if (!this.className) {
-      secElement.src = secElement.dataset.supriseSrc;
-      secElement.dataset.supriseSrc = tempSrc2;
-      secElement.classList.add("active");
-      secElement.innerText = "Close";
-      this.style.visibility = "0";
-    } else {
-      this.style.visibility = "1";
-      secElement.src = tempSrc2;
-      secElement.innerText = "Click Me";
-      secElement.classList.remove("active");
+      if (this.tagName == "IMG") {
+        button.classList.add("active");
+        button.innerText = "Close";
+      } else {
+        this.classList.add("active");
+        this.innerText = "Close";
+      }
+      secElement.src = secElement.getAttribute("data-suprise-src");
+      secElement.style.scale = "1.1";
+      secElement.setAttribute("data-suprise-src", tempSrc2);
+      setTimeout(() => {
+        secElement.style.scale = "1";
+      }, 700);
+
+      createAudio.autoplay = true;
+
+      if (createAudio.canPlayType("audio/mpeg")) {
+        const source = document.createElement("source");
+        source.setAttribute("src", "assets/audio/wow.mp3");
+        source.type = "audio/mpeg";
+        createAudio.appendChild(source);
+      }
+      main.appendChild(createAudio);
     }
+    setTimeout(() => {
+      if (this.tagName == "IMG") {
+        button.classList.remove("active");
+        button.innerText = "Click Me";
+      } else {
+        this.innerText = "Click Me";
+        this.classList.remove("active");
+      }
+      secElement.setAttribute("data-suprise-src", secElement.src);
+      secElement.src = tempSrc2;
+      createAudio.remove();
+    }, 2000);
   });
 }
